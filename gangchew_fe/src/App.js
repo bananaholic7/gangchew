72a09c700449c28d434b89e255f38448b3182597
JoHB94 <whgusqls113@gmail.com>
2023-09-25 12:44:23 +0900

    app set

--- gangchew_fe/src/App.js
+++ gangchew_fe/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    {/* 헤더 컴포넌트 들어갈 부분*/}
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        {/*홈*/}
+      </Routes>
+    {/* 푸터 컴포넌트 들어갈 부분*/}
+    </BrowserRouter>
+  </div>
   );
 }
 
