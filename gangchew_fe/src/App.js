64fcd46cc3571ea0a8f6e729a531d8d036f6c51e
Geon.C <chlrjs1324@gmail.com>
2023-09-26 12:16:05 +0900

    login page designed

--- gangchew_fe/src/App.js
+++ gangchew_fe/src/App.js
@@ -1,21 +1,23 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Header from './main/Header';
-import FundingCreate from './funding/FundingCreate';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./main/Header";
+import FundingCreate from "./funding/FundingCreate";
+import Login from "./member/Login";
 
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-      <Header/>
-      <Routes>
-       {/* <Route exact path='/' element={<Home/>}/> */} 
-        {/*홈*/}
-        <Route exact path='/fundingcreate' element={<FundingCreate/>}/>
-      </Routes>
-    {/* 푸터 컴포넌트 들어갈 부분*/}
-    </BrowserRouter>
-  </div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          {/* <Route exact path='/' element={<Home/>}/> */}
+          {/*홈*/}
+          <Route exact path="/fundingcreate" element={<FundingCreate />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+        {/* 푸터 컴포넌트 들어갈 부분*/}
+      </BrowserRouter>
+    </div>
   );
 }
 
